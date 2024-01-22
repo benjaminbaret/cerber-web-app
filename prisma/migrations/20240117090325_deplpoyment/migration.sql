c6a5e261d3e73a9ea2c9e6fc26fe69c6a2080478
33786 <adrien.ravel@edu.ece.fr>
2024-01-22 09:37:45 +0100

    feat(backend): database & signin / signup

--- prisma/migrations/20240117090325_deplpoyment/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "deployments" DROP CONSTRAINT "deployments_groupId_fkey";
-
--- AddForeignKey
-ALTER TABLE "deployments" ADD CONSTRAINT "deployments_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "groups"("id") ON DELETE CASCADE ON UPDATE CASCADE;
