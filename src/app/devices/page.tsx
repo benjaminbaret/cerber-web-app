d9c610f41bdd4ebec52978cf921dff3d7306d065
33786 <adrien.ravel@edu.ece.fr>
2024-01-23 20:21:35 +0100

    Les filtres marchent

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -1,78 +1,59 @@
 "use client"
-import Navbar from "../component/header/navbar";
+
 import React, { useEffect, useState } from 'react';
-import Footer from "../component/footer/footer";
-import PopUpNewDevice from "../component/popupnewdevice/page";
-import PopUpNewGroup from "../component/popupnewgroup/page";
-import PopUpDeleteDevice from "../component/popupdeletedevice/page";
-import  displayContent from "../component/displayContent/displayDevices";
+import Navbar from '../component/header/navbar';
+import Footer from '../component/footer/footer';
+import PopUpNewDevice from '../component/popupnewdevice/page';
+import PopUpNewGroup from '../component/popupnewgroup/page';
+import PopUpDeleteDevice from '../component/popupdeletedevice/page';
 import SearchIcon from '@mui/icons-material/Search';
+import DisplayContent from '../component/displayContent/displayDevices';
 
-const selectAll = () => {
-    const inputElement = document.getElementById('searchInputStatus') as HTMLInputElement;
-    if (inputElement) {
-        inputElement.checked = !inputElement.checked;
-        if (inputElement.checked) {
-            console.log("Il faut cocher toutes les cases");
-            const elements = document.querySelectorAll<HTMLInputElement>('[id^="select"]');
-            elements.forEach((element) => {
-                element.checked = true;
-            });
+const DevicesPage = () => {
+    // Utilisez le state pour stocker les valeurs
+    const [groupValue, setGroupValue] = useState('');
+    const [typeValue, setTypeValue] = useState('');
+    const [statusValue, setStatusValue] = useState('');
+    const [searchNameValue, setSearchNameValue] = useState('');
 
-        } else {
-            console.log("Il faut décocher toutes les cases");
-            const elements = document.querySelectorAll<HTMLInputElement>('[id^="select"]');
-            elements.forEach((element) => {
-                element.checked = false;
-            });
-        }
-    } 
-};
+    // Utilisez useEffect pour définir l'intervalle et mettre à jour les valeurs toutes les secondes
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // Mettez à jour les valeurs en fonction des éléments du DOM
+            setGroupValue(changeGroup());
+            setTypeValue(changeType());
+            setStatusValue(changeStatus());
+            setSearchNameValue(inputSearchName());
+        }, 1000);
 
-const devicesPage = () => {
-    const changeSQLRequest = () => {
-        let requeteSQL = ''; // Pas besoin de virgule ici
-        let compteur = 0;
-        const changeGroup = document.getElementById('inputGroup') as HTMLInputElement;
-        const changeType = document.getElementById('searchInputType') as HTMLInputElement;
-        const changeStatus = document.getElementById('searchInputStatus') as HTMLInputElement;
-        const inputSearchName = document.getElementById('searchInputName') as HTMLInputElement;
+        // Nettoyez l'intervalle lorsque le composant est démonté
+        return () => clearInterval(intervalId);
+    }, []);
 
-        if (inputSearchName && inputSearchName.value !== '') {
-            if (compteur === 0) {
-                requeteSQL = requeteSQL + ".eq('name', '" + inputSearchName + "')";
-                compteur++;
-            } else {
-                requeteSQL = requeteSQL + ".and('name', 'eq', '" + inputSearchName + "')";
-            }
-        }
-        if (changeType && changeType.value !== '' && changeType.value !== 'all') {
-            if (compteur === 0) {
-                requeteSQL = requeteSQL + ".eq('type', '" + changeType + "')";
-                compteur++;
-            } else {
-                requeteSQL = requeteSQL + ".and('type', 'eq', '" + changeType + "')";
-            }
-        }
-        if (changeGroup && changeGroup.value !== '' && changeGroup.value !== 'all') {
-            if (compteur === 0) {
-                requeteSQL = requeteSQL + ".eq('group', '" + changeGroup.value + "')";
-                compteur++;
-            } else {
-                requeteSQL = requeteSQL + ".and('group', 'eq', '" + changeGroup.value + "')";
-            }
-        }
-        if (changeStatus && changeStatus.value !== '' && changeStatus.value !== 'all') {
-            if (compteur === 0) {
-                requeteSQL = requeteSQL + ".eq('deviceStatus', '" + changeStatus.value + "')";
-                compteur++;
-            } else {
-                requeteSQL = requeteSQL + ".and('deviceStatus', 'eq', '" + changeStatus.value + "')";
-            }
-        }
-        console.log(requeteSQL);
-        return requeteSQL;
-    }
+    const selectAll = () => {
+        // ... (votre implémentation de selectAll reste inchangée)
+    };
+
+    const changeGroup = () => {
+        const changeGroupElement = document.getElementById('inputGroup') as HTMLInputElement;
+        return changeGroupElement ? changeGroupElement.value : '';
+    };
+
+    const changeType = () => {
+        const changeTypeElement = document.getElementById('searchInputType') as HTMLInputElement;
+        return changeTypeElement ? changeTypeElement.value : '';
+    };
+
+    const changeStatus = () => {
+        const changeStatusElement = document.getElementById('searchInputStatus') as HTMLInputElement;
+        return changeStatusElement ? changeStatusElement.value : '';
+    };
+
+    const inputSearchName = () => {
+        const inputSearchNameElement = document.getElementById('searchInputName') as HTMLInputElement;
+        return inputSearchNameElement ? inputSearchNameElement.value : '';
+    };
+
     return (
         <div className="bg-darkPurple text-white">
             <Navbar currentPage="devices" />
@@ -86,13 +67,13 @@ const devicesPage = () => {
             <div id="pageContent" className="w-full bg-darkPurple text-white justify-between items-center min-h-screen">
                 <table className="w-full mt-5 mb-6 justify-between items-center h-full relative ">
                     <thead className="h-16 w-full bg-darkPurple z-50 bg-intermediatePurple text-md">
-                        <tr className="w-full bg-darkPurple z-50 bg-intermediatePurple text-md">
-                            <th key="column1" className="w-1/7"> 
-                                <div>Select All</div>
-                                <div className="flex items-center justify-center pt-2 pb-2">
-                                    <input onChange={selectAll} type="checkbox" id="selectAll" name="selectAll"/>
-                                </div>
-                            </th>
+                    <tr className="w-full bg-darkPurple z-50 bg-intermediatePurple text-md">
+                        <th key="column1" className="w-1/7">
+                            <div>Select All</div>
+                            <div className="flex items-center justify-center pt-2 pb-2">
+                                <input onChange={selectAll} type="checkbox" id="selectAll" name="selectAll" />
+                            </div>
+                        </th>
                             <th key="column2" className="w-1/7">
                                 <div className="flex flex-col items-center justify-center">
                                     <div>Status</div>
@@ -161,11 +142,11 @@ const devicesPage = () => {
                             </th>
                         </tr>
                     </thead>
-                    {displayContent()}
+                    {DisplayContent(groupValue, typeValue, statusValue, searchNameValue)}
                 </table>
             </div>
-        <Footer />     
-    </div>
+            <Footer />
+        </div>
     );
 };
-export default devicesPage;
+export default DevicesPage;
