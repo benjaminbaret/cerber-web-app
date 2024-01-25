7fc6abeba17fa282233d4f6d6dbabbd4b3724db1
MargauxLeglise <91328888+MargauxLeglise@users.noreply.github.com>
2024-01-25 10:12:39 +0100

    historic

--- src/app/component/displayContent/displayHistoric.tsx
+++ src/app/component/displayContent/displayHistoric.tsx
@@ -1,28 +1,81 @@
 "use client";
-
+import CircleIcon from '@mui/icons-material/Circle';
 import supabase from '../../connexionDatabase/connectToDatabase';
 import React, { useEffect, useState } from 'react';
 
 
-const DisplayContent = () => {
+const DisplayContent = (changeStatusValue, searchDeviceNameValue, searchUpdateIdValue, changeGroupNameValue, changeTypeValue) => {
     ///TODO gerer les online offline
     const [data, setData] = useState<any[] | null>(null);
     const [error, setError] = useState<any | null>(null);
 
+    const fillTableau2D = (changeStatusValue, searchDeviceNameValue, searchUpdateIdValue, changeGroupNameValue, changeTypeValue) => {
+        const tableau2D = [];
+
+        if (changeStatusValue && changeStatusValue !== 'all' && cchangeStatusValue !== '') {
+            tableau2D.push(['status', changeStatusValue]);
+        } else {
+            tableau2D.push(['status', '']);
+        }
+
+        if (searchDeviceNameValue && searchDeviceNameValue !== '') {
+            tableau2D.push(['device name', searchDeviceNameValue]);
+        } else {
+            tableau2D.push(['device name', '']);
+        }
+
+        if (searchUpdateIdValue && searchUpdateIdValue !== '') {
+            tableau2D.push(['update Id', searchUpdateIdValue]);
+        } else {
+            tableau2D.push(['update Id', '']);
+        }
+
+        if (changeGroupNameValue && changeGroupNameValue !== 'all' && changeGroupNameValue !== '') {
+            tableau2D.push(['group name', changeGroupNameValue]);
+        } else {
+            tableau2D.push(['group name', '']);
+        }
+
+        if (changeTypeValue && changeTypeValue !== 'all' && cchangeTypeValue !== '') {
+            tableau2D.push(['type', changeTypeValue]);
+        } else {
+            tableau2D.push(['type', '']);
+        }
+        return tableau2D;
+    };
+
     useEffect(() => {
         const fetchData = async () => {
-            
+            const SQLRequest = fillTableau2D(changeGroupValue,changeTypeValue,changeStatusValue,inputSearchNameValue);
+            console.log(SQLRequest);
             try {
-                const { data, error } = await supabase.from('historic').select('*,devices(updateStatus), devices(name), updates(id),groups(name), devices(type)'); //Data en local
+                let query = supabase.from('devices').select('*');
+                for (const condition of SQLRequest) {
+                    if (condition[1] !== "") {
+                        query = query.eq(condition[0], condition[1]);
+                    }
+                }
 
-                setData(data);
-
+                const { data, error } = await query;
+                if (error) {
+                    setError(error);
+                } else {
+                    setData(data);
+                }
             } catch (error) {
                 setError(error);
             }
         };
         fetchData();
-    },[]);
+    }, [changeGroupValue, changeTypeValue, changeStatusValue, inputSearchNameValue]);
+
+    const check = () => {
+        const inputElement = document.getElementById('selectAll') as HTMLInputElement;
+        if (inputElement) {
+            inputElement.checked = false;
+        }
+    };
+
 
     if (error) {
         console.error('Erreur lors de la récupération des données :', error);
@@ -31,33 +84,32 @@ const DisplayContent = () => {
 
     return (
         <tbody>
-            {data?.map((historic) => (
-                // eslint-disable-next-line react/jsx-key
-
-                <tr className="relative" key={historic.id}>
-                    <td className="text-center w-1/5">
-                    {historic.devices.updateStatus}
-                    </td>
-
-                    <td className="text-center w-1/5">
-                    {historic.devices.name}
-                    </td>
-
-                    <td className="text-center w-1/5">
-                    {historic.updates.id}
-                    </td>
-
-                    <td className="text-center w-1/5">
-                    {historic.groupId ? historic.groups.name : 'N/A'}
-
-                    </td>
-
-                    <td className="text-center w-1/5">
-                    {historic.devices.type}
-
-                    </td>
-
-                    <style jsx>{`
+        {data?.map((device) => (
+            // eslint-disable-next-line react/jsx-key
+            <tr className="relative" key={device.id}>
+                <td className="text-center w-1/7">
+                    <input type="checkbox" onChange={check} id={"select" + device.id} name={"select" + device.id} />
+                </td>
+                <td id={"Status" + device.id} className="pb-3 pt-3 flex items-center justify-center text-center w-1/7">
+                    {processUpdate(device.updatedAt, device.deviceStatus)}
+                </td>
+                <td className="text-center w-1/7">
+                    {device.name}
+                </td>
+                <td className="text-center w-1/7">
+                    {device.type}
+                </td>
+                <td className="text-center w-1/7">
+                    {device.group ? device.group.name : 'N/A'}
+                </td>
+                <td className="text-center w-1/7">
+                    Original
+                </td>
+                <td className="text-center w-1/7">
+                    {/*TODO RECUPERER CELA*/}
+                    Last Update
+                </td>
+                <style jsx>{`
                             tr::after {
                                 content: "";
                                 position: absolute;
@@ -68,12 +120,10 @@ const DisplayContent = () => {
                                 background-color: #e2e8f0;
                                 opacity: 0.28;
                             }
-                    `}</style>
-
-                </tr>
-            ))}
+                        `}</style>
+            </tr>
+        ))}
         </tbody>
     );
-    };
-
+};
 export default DisplayContent;
